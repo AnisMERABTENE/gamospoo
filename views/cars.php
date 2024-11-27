d28bbfa9dd935aa26c7ba95124ef6d7e8bac6868
Harold <harolddev75@gmail.com>
2024-11-27 14:53:49 +0100

    add div and select

--- views/cars.php
+++ views/cars.php
@@ -1,15 +1,79 @@
-<?php ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Voiture disponible</h1>
+<?php
 
-    efjeqroighzroùigh   zroI%variant_neg
-    VZERgZRGzrgZ
-</body>
-</html>
+$title = "cars";
+
+?>
+<style>
+    .centertop {
+        display: flex;
+        flex-wrap: wrap;
+
+
+        height: 200px;
+        width: 1200vh;
+        align-items: center;
+        background-color: red;
+    }
+</style>
+<!-- SELECT Bootstrap -->
+<div class="centertop">
+    <select class="form-select form-select-sm" aria-label="Small select example">
+        <option selected>Prix</option>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+    </select>
+
+    <select class="form-select form-select-sm" aria-label="Small select example">
+        <option selected>Marque</option>
+        <option value="1">Merco</option>
+        <option value="2">Féfé</option>
+        <option value="3">Lambo</option>
+    </select>
+    <select class="form-select form-select-sm" aria-label="Small select example">
+        <option selected>Carburant</option>
+        <option value="1">Essence SP 97</option>
+        <option value="2">Diesel</option>
+        <option value="3">Hydrogène</option>
+        <option value="4">Electrique</option>
+    </select>
+    <select class="form-select form-select-sm" aria-label="Small select example">
+        <option selected>Transmission</option>
+        <option value="1">Manuelle</option>
+        <option value="2">Automatique</option>
+    </select>
+    <select class="form-select form-select-sm" aria-label="Small select example">
+        <option selected>Siège</option>
+        <option value="1">2</option>
+        <option value="2">3</option>
+        <option value="3">4</option>
+        <option value="4">5</option>
+        <option value="5">6</option>
+        <option value="6">7</option>
+    </select>
+    <!-- GRID bootstrap-->
+
+    <div class="container text-center">
+        <div class="row">
+            <div class="col">PICTURE'S GAMOS</div>
+            <div class="col">PICTURE'S GAMOS</div>
+            <div class="col">PICTURE'S GAMOS</div>
+        </div>
+        <div class="row">
+            <div class="col">PICTURE'S GAMOS</div>
+            <div class="col">PICTURE'S GAMOS</div>
+            <div class="col">PICTURE'S GAMOS</div>
+        </div>
+        <div class="row">
+            <div class="col">PICTURE'S GAMOS</div>
+            <div class="col">PICTURE'S GAMOS</div>
+            <div class="col">PICTURE'S GAMOS</div>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+
+<?php
+
+?>
