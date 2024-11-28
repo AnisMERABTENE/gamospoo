d539ff2c5521c4d65fdb33017a7001021277be21
Harold <harolddev75@gmail.com>
2024-11-28 16:19:53 +0100

    doublons supprimés

--- views/mesReservation.html.php
+++ views/mesReservation.html.php
@@ -3,6 +3,7 @@
 ?>
 
 <div class="main-container">
+
     <div class="content-container">
         <h2>Mes Réservations</h2>
 
@@ -22,35 +23,7 @@
                             <tr>
                                 <td><?= date("d/m/Y", strtotime($reservation['date_debut'])) ?></td>
                                 <td><?= date("d/m/Y", strtotime($reservation['date_fin'])) ?></td>
-                                <td><?= $reservation['prix_total']."€" ?></td>
-                            </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
-            </div>
-        <?php else: ?>
-            <p>Aucune réservation en cours.</p>
-        <?php endif; ?>
-    <div class="content-container">
-        <h2>Mes Réservations</h2>
-
-        <h3>Réservations en cours :</h3>
-        <?php if (!empty($reservations['En cours'])): ?>
-            <div class="table-container">
-                <table class="reservation-table">
-                    <thead>
-                        <tr>
-                            <th>Date de début</th>
-                            <th>Date de fin</th>
-                            <th>Prix total</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($reservations['En cours'] as $reservation): ?>
-                            <tr>
-                                <td><?= date("d/m/Y", strtotime($reservation['date_debut'])) ?></td>
-                                <td><?= date("d/m/Y", strtotime($reservation['date_fin'])) ?></td>
-                                <td><?= $reservation['prix_total']."€" ?></td>
+                                <td><?= $reservation['prix_total'] . "€" ?></td>
                             </tr>
                         <?php endforeach; ?>
                     </tbody>
@@ -77,7 +50,7 @@
                             <tr>
                                 <td><?= date("d/m/Y", strtotime($reservation['date_debut'])) ?></td>
                                 <td><?= date("d/m/Y", strtotime($reservation['date_fin'])) ?></td>
-                                <td><?= $reservation['prix_total']."€" ?></td>
+                                <td><?= $reservation['prix_total'] . "€" ?></td>
                                 <td>
                                     <form action="" method="POST">
                                         <input type="hidden" name="id_reservation" value="<?= $reservation['id_reservation'] ?>" />
@@ -93,7 +66,7 @@
             <p>Aucune réservation à venir.</p>
         <?php endif; ?>
 
-       
+
         <h3>Réservations passées :</h3>
         <?php if (!empty($reservations['Passee'])): ?>
             <div class="table-container">
@@ -110,16 +83,14 @@
                             <tr>
                                 <td><?= date("d/m/Y", strtotime($reservation['date_debut'])) ?></td>
                                 <td><?= date("d/m/Y", strtotime($reservation['date_fin'])) ?></td>
-                                <td><?= $reservation['prix_total']."€" ?></td>
+                                <td><?= $reservation['prix_total'] . "€" ?></td>
                             </tr>
                         <?php endforeach; ?>
                     </tbody>
                 </table>
-             </div>
-           <?php else: ?>
+            </div>
+        <?php else: ?>
             <p>Aucune réservation passée.</p>
         <?php endif; ?>
     </div>
 </div>
-
-
